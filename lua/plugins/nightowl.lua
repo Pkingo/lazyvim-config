0d34d2095e7b019df4bb65a15193d68d258f5d2d
Philip Kingo <philipandreasking@hotmail.com>
2023-09-05 12:37:53 +0200

    update themes

--- lua/plugins/nightowl.lua
+++
@@ -1,5 +0,0 @@
-return {
-  {
-    "oxfist/night-owl.nvim",
-  },
-}
---
+++ lua/plugins/theme.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "oxfist/night-owl.nvim",
+    lazy = true, -- make sure we load this during startup if it is your main colorscheme
+  },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+  },
+}
