845801f126c0f3eba11331b3836a8c3fc160d878
Philip Kingo <philipandreasking@hotmail.com>
2023-09-13 10:36:35 +0200

    update config

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,11 +1,12 @@
 return {
   {
-    "oxfist/night-owl.nvim",
-    lazy = true, -- make sure we load this during startup if it is your main colorscheme
-  },
-  {
-    "catppuccin/nvim",
-    lazy = true,
-    name = "catppuccin",
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
   },
 }
