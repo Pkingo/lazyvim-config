952d7d96216624cb86759b716e5c1ab3a7a3d28e
Philip Kingo <philipandreasking@hotmail.com>
2023-09-16 12:16:01 +0200

    update go config

--- lua/plugins/blamer.lua
+++ lua/plugins/blamer.lua
@@ -1,3 +1,13 @@
+-- Show Git blame inline
+-- https://github.com/APZelos/blamer.nvim
 return {
-  { "APZelos/blamer.nvim" },
+  "APZelos/blamer.nvim",
+  keys = {
+	-- stylua: ignore
+    { "<leader>gB", "<cmd>BlamerToggle<cr>", desc = "Git Blame" },
+  },
+  config = function()
+    vim.g.blamer_enabled = 1
+    vim.g.blamer_delay = 200
+  end,
 }
--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -4,6 +4,14 @@ return {
     opts = function(_, opts)
       local nls = require("null-ls")
       table.insert(opts.sources, nls.builtins.formatting.prettierd)
+      if type(opts.sources) == "table" then
+        vim.list_extend(opts.sources, {
+          nls.builtins.code_actions.gomodifytags,
+          nls.builtins.code_actions.impl,
+          nls.builtins.formatting.gofumpt,
+          nls.builtins.formatting.goimports_reviser,
+        })
+      end
     end,
   },
 }
